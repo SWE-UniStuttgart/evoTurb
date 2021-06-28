5e86626eb7d296256608e1c56b75164eca8c8437
Feng Guo <feng.guo@hs-flensburg.de>
2021-06-28 13:07:06 +0200

    Update TestCoherence.m
    added legend to the bottom figure (left);
    added full size window for the plot;
    set the legend locations

--- evoTurb_matlab/example/TestCoherence.m
+++ evoTurb_matlab/example/TestCoherence.m
@@ -120,57 +120,87 @@ end
 
 % calculate analytical longitudinal coherence
 [Cohx_u,ConfigParameters] = CalcCohx(ConfigParameters); 
+x_coh1                    = interp1(ConfigParameters.f,squeeze(Cohx_u(1,2,:)),f_est,'linear','extrap');  % theorectical coherence Dx =50m
+x_coh2                    = interp1(ConfigParameters.f,squeeze(Cohx_u(1,3,:)),f_est,'linear','extrap');  % theorectical coherence Dx =100m
+
+
+
+
 
 %% plot
-figure(1)
+set(groot,'defaultTextInterpreter','latex')
+set(groot,'defaultAxesTickLabelInterpreter','latex')
+set(groot,'defaultLegendInterpreter','latex')
+set(groot,'defaultFigureColor','w')
+set(groot,'defaultTextFontSize',10)
+set(groot,'defaultAxesFontSize',10)
+set(groot,'defaultLineLineWidth',1.2)
+
+figure('units','normalized','outerposition',[0 0 1 1])
 subplot(2,2,1)
 hold on; grid on; box on;
-plot(f_est,abs(mean(CPSD_1_1_1_to_1_1_2,1).^2)./mean(PSD_1_1_1,1)./mean(PSD_1_1_2,1));
-plot(f_est,abs(mean(CPSD_1_1_1_to_1_1_3,1).^2)./mean(PSD_1_1_1,1)./mean(PSD_1_1_3,1));
+plot(f_est,abs(mean(CPSD_1_1_1_to_1_1_2,1).^2)./mean(PSD_1_1_1,1)./mean(PSD_1_1_2,1),'-.','color',[0 0.4470 0.7410]);
+plot(f_est,abs(mean(CPSD_1_1_1_to_1_1_3,1).^2)./mean(PSD_1_1_1,1)./mean(PSD_1_1_3,1),'-.','color',[0.8500 0.3250 0.0980]);
 % plot(f_est,abs(mean(CPSD_1_1_2_to_1_1_3,1).^2)./mean(PSD_1_1_2,1)./mean(PSD_1_1_3,1));
-plot(ConfigParameters.f,squeeze(Cohx_u(1,2,:)).^2)
-plot(ConfigParameters.f,squeeze(Cohx_u(1,3,:)).^2)
-legend('estimated: Dx = 50 m','estimated: Dx = 100 m est','theoretical: Dx = 50 m','theoretical: Dx = 100 m')
+plot(f_est,x_coh1.^2,'color',[0 0.4470 0.7410])
+plot(f_est,x_coh2.^2,'color',[0.8500 0.3250 0.0980])
+legend('estimated: Dx = 50 m','estimated: Dx = 100 m','theoretical: Dx = 50 m','theoretical: Dx = 100 m','Location','northeast')
 set(gca,'Xscale','log')
 xlabel('frequency [Hz]')
 ylabel('magnitude coherence [-]')
 
 subplot(2,2,2)
 hold on; grid on; box on;
-plot(f_est,abs(mean(CPSD_1_1_1_to_1_4_1,1).^2)./mean(PSD_1_1_1,1)./mean(PSD_1_4_1,1));
-plot(f_est,abs(mean(CPSD_1_1_1_to_1_8_1,1).^2)./mean(PSD_1_1_1,1)./mean(PSD_1_8_1,1));
+plot(f_est,abs(mean(CPSD_1_1_1_to_1_4_1,1).^2)./mean(PSD_1_1_1,1)./mean(PSD_1_4_1,1),'-.','color',[0 0.4470 0.7410]);
+plot(f_est,abs(mean(CPSD_1_1_1_to_1_8_1,1).^2)./mean(PSD_1_1_1,1)./mean(PSD_1_8_1,1),'-.','color',[0.8500 0.3250 0.0980]);
 if strcmp(ConfigParameters.TurbModel,'Kaimal')   
     yz_coh1      = exp(-12*Dy*4*((f_est/ConfigParameters.Uref).^2+(0.12/ConfigParameters.L_u).^2).^0.5);
     yz_coh2      = exp(-12*Dy*8*((f_est/ConfigParameters.Uref).^2+(0.12/ConfigParameters.L_u).^2).^0.5);
-    plot(f_est,yz_coh1.^2);
-    plot(f_est,yz_coh2.^2);
+    plot(f_est,yz_coh1.^2,'color',[0 0.4470 0.7410]);
+    plot(f_est,yz_coh2.^2,'color',[0.8500 0.3250 0.0980]);
     set(gca,'Xscale','log')
     xlabel('frequency [Hz]')
     ylabel('magnitude coherence [-]')
     title('Dz = 0')
-    legend('estimated: Dy = 16 m','estimated: Dy = 32 m','theoretical: Dy = 16 m','theoretical: Dy = 32 m')
+    legend('estimated: Dy = 16 m','estimated: Dy = 32 m','theoretical: Dy = 16 m','theoretical: Dy = 32 m','Location','northeast')
 else
     load('example\MannCoh.mat') 
     yz_coh1          = interp1(k_coh/2/pi*ConfigParameters.Uref,coh_1,f_est,'linear','extrap');
     yz_coh2          = interp1(k_coh/2/pi*ConfigParameters.Uref,coh_2,f_est,'linear','extrap');
-    plot(f_est,yz_coh1.^2);
-    plot(f_est,yz_coh2.^2);
+    plot(f_est,yz_coh1.^2,'color',[0 0.4470 0.7410]);
+    plot(f_est,yz_coh2.^2,'color',[0.8500 0.3250 0.0980]);
     set(gca,'Xscale','log')
     xlabel('frequency [Hz]')
     ylabel('magnitude coherence [-]')
     title('Dz = 0')
-    legend('estimated: Dy = 12 m','estimated: Dy = 28 m','theoretical: Dy = 12 m','theoretical: Dy = 28 m')
+    legend('estimated: Dy = 12 m','estimated: Dy = 28 m','theoretical: Dy = 12 m','theoretical: Dy = 28 m','Location','northeast')
 end    
 
 subplot(2,2,3)
 hold on; grid on; box on;
-plot(f_est,abs(mean(CPSD_1_4_1_to_1_1_2,1).^2)./mean(PSD_1_4_1,1)./mean(PSD_1_1_2,1));
-plot(f_est,abs(mean(CPSD_1_4_1_to_1_1_3,1).^2)./mean(PSD_1_4_1,1)./mean(PSD_1_1_3,1));
-plot(f_est,abs(mean(CPSD_1_8_1_to_1_1_2,1).^2)./mean(PSD_1_8_1,1)./mean(PSD_1_1_2,1));
-plot(f_est,abs(mean(CPSD_1_8_1_to_1_1_3,1).^2)./mean(PSD_1_8_1,1)./mean(PSD_1_1_3,1));
+plot(f_est,abs(mean(CPSD_1_4_1_to_1_1_2,1).^2)./mean(PSD_1_4_1,1)./mean(PSD_1_1_2,1),'-.','color',[0 0.4470 0.7410]);
+plot(f_est,abs(mean(CPSD_1_4_1_to_1_1_3,1).^2)./mean(PSD_1_4_1,1)./mean(PSD_1_1_3,1),'-.','color',[0.8500 0.3250 0.0980]);
+plot(f_est,abs(mean(CPSD_1_8_1_to_1_1_2,1).^2)./mean(PSD_1_8_1,1)./mean(PSD_1_1_2,1),'-.','color',[0.4660 0.6740 0.1880]);
+plot(f_est,abs(mean(CPSD_1_8_1_to_1_1_3,1).^2)./mean(PSD_1_8_1,1)./mean(PSD_1_1_3,1),'-.','color',[0.3010 0.7450 0.9330]);
+plot(f_est,(x_coh1.*yz_coh1).^2,'color',[0 0.4470 0.7410])
+plot(f_est,(x_coh2.*yz_coh1).^2,'color',[0.8500 0.3250 0.0980])
+plot(f_est,(x_coh1.*yz_coh2).^2,'color',[0.4660 0.6740 0.1880])
+plot(f_est,(x_coh2.*yz_coh2).^2,'color',[0.3010 0.7450 0.9330])
 set(gca,'Xscale','log')
 xlabel('frequency [Hz]')
 ylabel('magnitude coherence [-]')
+title('Dz = 0')
+if strcmp(ConfigParameters.TurbModel,'Kaimal')  
+   legend('estimated: Dx = 50 m, Dy = 16 m','estimated: Dx = 100 m, Dy = 16 m',...
+          'estimated: Dx = 50 m, Dy = 32 m','estimated: Dx = 100 m, Dy = 32 m',...
+          'theoretical: Dx = 50 m, Dy = 16 m','theoretical: Dx = 100 m, Dy = 16 m',...
+          'theoretical: Dx = 50 m, Dy = 32 m','theoretical: Dx = 100 m, Dy = 32 m','Location','northeast')
+else
+   legend('estimated: Dx = 50 m, Dy = 12 m','estimated: Dx = 100 m, Dy = 12 m',...
+          'estimated: Dx = 50 m, Dy = 28 m','estimated: Dx = 100 m, Dy = 28 m',...
+          'theoretical: Dx = 50 m, Dy = 12 m','theoretical: Dx = 100 m, Dy = 12 m',...
+          'theoretical: Dx = 50 m, Dy = 28 m','theoretical: Dx = 100 m, Dy = 28 m','Location','northeast')
+end
 
 subplot(2,2,4)
 hold on; grid on; box on;
@@ -180,7 +210,7 @@ plot(f_est,f_est'.*mean(PSD_w_1_1_3,1));
 plot(f_est,f_est'.*mean(real(CPSD_1_1_3_to_w_1_1_3),1));
 set(gca,'Xscale','log')
 xlabel('frequency [Hz]')
-ylabel('spectra [(m/s)^2/Hz]')
-legend('u','v','w','uw')
+ylabel('$f\cdot$spectra [(m/s)$^2$]')
+legend('u','v','w','uw','Location','northeast')
 
 
